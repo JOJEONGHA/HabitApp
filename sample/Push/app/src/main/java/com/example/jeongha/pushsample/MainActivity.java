b4055c263505851fe33a5f17a73d1585117a3c92
MongEe <wosd1233@gmail.com>
2019-09-08 18:28:12 +0900

    push message 출력

--- sample/Push/app/src/main/java/com/example/jeongha/pushsample/MainActivity.java
+++ sample/Push/app/src/main/java/com/example/jeongha/pushsample/MainActivity.java
@@ -2,19 +2,52 @@ package com.example.jeongha.pushsample;
 
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-    Notification notify = new Notification(android.R.drawable.ic_lock_idle_alarm,"알람!!",System.currentTimeMillis());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+
+        String push_title = getResources().getString(R.string.channel_name);
+        String push_des = getResources().getString(R.string.channel_description);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "1004")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(push_title)
+                .setContentText(push_des)
+//                .setStyle(new NotificationCompat.BigTextStyle()
+//                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(110, builder.build());
+    }
+
+    // Create Notification Channel
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("1004", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
