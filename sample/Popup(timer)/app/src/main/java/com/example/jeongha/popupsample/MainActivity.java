3d16392d00b90529ec6a30442ffd7c44b1e54618
MongEe <wosd1233@gmail.com>
2019-09-24 12:39:36 +0900

    testing import

--- sample/Popup(timer)/app/src/main/java/com/example/jeongha/popupsample/MainActivity.java
+++ sample/Popup(timer)/app/src/main/java/com/example/jeongha/popupsample/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTick(long millisUntilFinished) {
-
+                System.out.printf("testing");
             }
 
             public void onFinish() {
