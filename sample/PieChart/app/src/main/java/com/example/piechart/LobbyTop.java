0b092e57c9266e7bc9a5fb7956648c38962d3132
JeongHAHAHA <wosd1233@gmail.com>
2019-10-08 00:00:04 +0900

    date

--- sample/PieChart/app/src/main/java/com/example/piechart/LobbyTop.java
+++ sample/PieChart/app/src/main/java/com/example/piechart/LobbyTop.java
@@ -7,6 +7,10 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,6 +18,9 @@ import androidx.fragment.app.Fragment;
 public class LobbyTop extends Fragment {
     TextView midlle_text;
     ImageButton left_btn, right_btn;
+    String getDate;
+    Date date;
+    Calendar cal;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,11 +36,24 @@ public class LobbyTop extends Fragment {
         right_btn = (ImageButton) view.findViewById(R.id.lobby_rightBtn);
         midlle_text = (TextView) view.findViewById(R.id.lobby_middleText);
 
+        // text 현재 날짜로 초기화
+        // 특정 String type으로 전환
+        date = new Date();
+        cal = Calendar.getInstance();
+        cal.setTime(date);
+
+        SimpleDateFormat date_form = new SimpleDateFormat("yy.MM.dd");
+        getDate = '\''+  date_form.format(cal.getTime());
+
+        // 현재 날짜로 초기화
+        midlle_text.setText(getDate);
+
         left_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // chart 혹은 main으로 중간 날짜정보 전송
-                // 날짜 변경
+                // 날짜 - 1 로 변경
+                // 날짜 - 7이상 넘어가지 않게 조절
             }
         });
 
@@ -42,6 +62,8 @@ public class LobbyTop extends Fragment {
             public void onClick(View v) {
                 // chart 혹은 main으로 중간 날짜정보 전송
                 // 날짜 변경
+                // 날짜 + 1
+                // 현재 날짜를 넘어가지 않게
             }
         });
 
